9e97312a60dae16ea4fcbcb26c99b4979f46f705
tkuriyama <taro.kuriyama@gmail.com>
2021-12-01 16:22:34 -0500

    Add validation checks

--- python-indexer/src/validate_index.py
+++ python-indexer/src/validate_index.py
@@ -11,12 +11,60 @@ from typing import Dict, List, Tuple # type: ignore
 ################################################################################
 
 
-def test_find_self(pkg_ref: List[PackageListing],
+def test_find_self(pkg_listings: List[PackageListing],
                    pkg_indexes: PackageIndexMap):
-    """Test if queryng by package name yields itself as the best result."""
-    pass
+    """Test if queryng by package name /desc  yields self as the best result."""
+    print('\n\n> Running package validation...\n')
+    pkg_listings_ = [listing for listing in pkg_listings
+                     if listing[0] in pkg_indexes]
+
+    d = pkg_listings_to_dict(pkg_listings)
+
+    find_self_name(pkg_listings_, d, pkg_indexes)
+    find_self_desc(pkg_listings_, d, pkg_indexes)
 
 
+def find_self_name(pkg_listings: List[PackageListing],
+                   pkg_ref: Dict[int, Tuple[str, str]],
+                   pkg_indexes: PackageIndexMap):
+    """'Check if"author/name" query finds package as first result."""
+    print('Check if"author/name" query finds package.')
+
+    for i, author, name, _, _, desc in pkg_listings:
+        q_str = f'{author}/{name}'
+        top = query(pkg_ref, pkg_indexes, q_str)
+        if not top:
+            print(f'Empty query result for {i} {q_str}')
+            continue
+
+        first = top[0][0]
+        if i != first:
+            print(f'Expected {q_str}, found {pkg_ref[first]} as first result')
+
+
+def find_self_desc(pkg_listings: List[PackageListing],
+                   pkg_ref: Dict[int, Tuple[str, str]],
+                   pkg_indexes: PackageIndexMap):
+    """'Check if name + desc query finds package as first result."""
+    print('Check if name + desc query finds package.')
+
+    count_top_3 = 0
+    count_top_10 = 0
+    for i, author, name, _, _, desc in pkg_listings:
+        q_str = desc + ' ' + name
+        top = query(pkg_ref, pkg_indexes, q_str)
+        if not top:
+            print(f'Empty query result for {i} {q_str}')
+            continue
+
+        if i in [result[0] for result in top[:3]]:
+            count_top_3 += 1
+
+        if i in [result[0] for result in top[:10]]:
+            count_top_10 += 1
+
+    print(f'\tFound package in top 3: {count_top_3 / len(pkg_listings):.1%}')
+    print(f'\tFound package in top 10: {count_top_10 / len(pkg_listings):.1%}')
 
 
 ################################################################################
@@ -77,10 +125,17 @@ def score_similarity(q_terms: List[Tuple[str, float]],
 # Helpers
 
 
-def load_package_ref(fname) -> Dict[int, Tuple[str, str]]:
+def load_pkg_ref(fname) -> Dict[int, Tuple[str, str]]:
     """Load package listing."""
     with open(fname, 'r') as f:
         lines = [line.split('\t') for line in f.readlines()]
 
+    pkg_listings = [(int(i), author, name, url, version, desc)
+                    for i, author, name, url, version, desc in lines]
+    return pkg_listings_to_dict(pkg_listings)
+
+
+def pkg_listings_to_dict(pkg_listings: List[PackageListing]
+                        ) -> Dict[int, Tuple[str, str]]:
     return dict([(int(i), (author, name))
-                 for i, author, name, _, _, _ in lines])
+                 for i, author, name, _, _, _ in pkg_listings])
